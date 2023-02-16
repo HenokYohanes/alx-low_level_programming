faaa92701228e8662a7aa83044b2b6e0fe9c4aed
Henok Yohanes <henoksolomon951@gmail.com>
2023-02-16 04:14:47 -0800

    program prints a line by using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that uses a printf function
+ * Return: This return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
