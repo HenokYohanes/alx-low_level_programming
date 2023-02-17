b5749d53b0f3d63635b9d55638e4abcae2818451
Henok Yohanes <henoksolomon951@gmail.com>
2023-02-17 05:18:56 -0800

    3. alphABET task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ * Return: Return 0
+ */
+	int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+
+	{
+		putchar(letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
+
